57d9676eaac83ab6b125c83f4e4c20ffb756aba3
xtyl <13310050-xtyl@users.noreply.replit.com>
2023-08-13 16:10:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const flip = document.getElementById("flip");
+    const resultSpan = document.getElementById("result");
+    const coin = document.getElementById("coin");
+
+    flip.addEventListener("click", function () {
+      const result = Math.random() < 0.5 ? "Heads" : "Tails";
+      resultSpan.innerHTML = '';
+      setTimeout(function() {
+          resultSpan.innerHTML = result;
+          coin.src = result.toLowerCase() + ".png";
+        }, 250);
+    });
