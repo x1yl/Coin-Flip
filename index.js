06520ff5d8180fe8607c618ab91b567e5e4b6c69
x1yl <life23243@tutanota.com>
2023-08-13 16:48:03 +0000

    Formatted JavaScript and moved images into a folder called src

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const flip = document.getElementById("flip");
-    const resultSpan = document.getElementById("result");
-    const coin = document.getElementById("coin");
+const resultSpan = document.getElementById("result");
+const coin = document.getElementById("coin");
 
-    flip.addEventListener("click", function () {
-      const result = Math.random() < 0.5 ? "Heads" : "Tails";
-      resultSpan.innerHTML = '';
-      setTimeout(function() {
-          resultSpan.innerHTML = result;
-          coin.src = result.toLowerCase() + ".png";
-        }, 250);
-    });
+flip.addEventListener("click", function () {
+  const result = Math.random() < 0.5 ? "Heads" : "Tails";
+  resultSpan.innerHTML = "";
+  setTimeout(function () {
+    resultSpan.innerHTML = result;
+    coin.src = "src/" + result.toLowerCase() + ".png";
+  }, 250);
+});
